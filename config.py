f8a1e97f51e88b43578cfdecd94e0508696f5bb7
stevensZane <lasiebazoungoula@gmail.com>
2025-07-11 18:14:32 +0000

    openai api problem

---
+++ config.py
@@ -0,0 +1,4 @@
+OPENAI_API_KEY = None
+ELEVENLABS_API_KEY = "sk_247c673a28c0ad1ea990a3b1a7efabbdd2b3cf4e1a2c038e"
+ELEVENLABS_VOICE_ID = "FGY2WhTYpPnrIDTdsKH5"  # Get this from https://www.elevenlabs.io/
+OPENROUTER_API_KEY = "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"
